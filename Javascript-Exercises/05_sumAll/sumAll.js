00d2731271760d2dd69aae3f7e73779719ce2569
Kevin Miles <kev-miles@hotmail.com>
2022-04-30 15:40:42 +0100

    completed exercises 05 & 06

--- Javascript-Exercises/05_sumAll/sumAll.js
+++ Javascript-Exercises/05_sumAll/sumAll.js
@@ -1,6 +1,48 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
 
+    if(parametersAreValid(number1,number2))
+    {
+
+        var orderedParams = orderParameters(number1, number2);
+        var start = orderedParams[0];
+        var finish = orderedParams[1];
+
+        var acc = 0;
+
+        for(let i=start-1; i<finish; i++)
+        {
+            acc += (start+i);
+        }
+
+        return acc
+    }   
+
+    return "ERROR"
 };
+
+const parametersAreValid = function(...args) {
+
+    for(let i=args.length-1; i>=0; i--)
+    {
+        if(args[i] < 0 || typeof args[i] != typeof 1){
+            return false
+        }
+    }
+
+    return true;
+}
+
+const orderParameters = function(...args){
+
+    if(args[0] >= args[1])
+    {
+        var aux = args[0];
+        args[0] = args[1];
+        args[1] = aux;
+    }
+
+    return args;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
--- Javascript-Exercises/06_leapYears/leapYears.js
+++ Javascript-Exercises/06_leapYears/leapYears.js
@@ -1,6 +1,42 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if(yearIsValid(year))
+    {
+        if(isPosibleLeapYear(year))
+            return validateLeapYear(year);
+    }
+    
+    //else
+    return false;
 };
+
+const yearIsValid = function(year){
+    if(typeof year != typeof 1 || year < 0){
+        return false
+    }
+
+    //else
+    return true;
+}
+
+const isPosibleLeapYear = function(year){
+
+    if(year % 4 === 0)
+        return true;
+        
+    //else
+    return false;
+}
+
+const validateLeapYear = function(year){
+    if(year % 100 != 0)
+        return true;
+    else if(year % 400 === 0)
+        return true;
+    
+    //else
+    return false;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
