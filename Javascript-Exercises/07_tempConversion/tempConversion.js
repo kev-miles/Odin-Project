cc51defa066f1babed300d453f31c320941a4b3c
Kevin Miles <kev-miles@hotmail.com>
2022-05-01 09:43:32 +0100

    completed exercises 07 & 08

--- Javascript-Exercises/07_tempConversion/tempConversion.js
+++ Javascript-Exercises/07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  return roundNumber((temperature - 32) * (5/9));
 };
 
-const ctof = function() {
+const ctof = function(temperature) {
+  return roundNumber(temperature * 9/5 + 32);
+};
 
+
+const roundNumber = function(number){
+  return Math.round(number * 10) / 10
 };
 
 // Do not edit below this line
--- Javascript-Exercises/08_calculator/calculator.js
+++ Javascript-Exercises/08_calculator/calculator.js
@@ -1,25 +1,73 @@
-const add = function() {
+const add = function(...args) {
 	
+  var acc = 0;
+
+  for(let i=args.length-1; i>=0; i--)
+  {
+      acc += args[i];
+  }
+
+  return acc;
 };
 
-const subtract = function() {
+const subtract = function(...args) {
 	
+  var acc = args[0];
+
+  for(let i=1; i<args.length; i++)
+  {
+      acc -= args[i];
+  }
+
+  return acc;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  
+  var acc = 0;
+
+  for(let i=array.length-1; i>=0; i--)
+  {
+      acc = add(acc, array[i]);
+  }
+
+  return acc;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+
+  var acc = numbers[0];
+
+  for(let i=1; i<numbers.length; i++)
+  {
+      acc *= numbers[i];
+  }
+
+  return acc;
 
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  
+  var acc = 1;
+
+  for(let i=0; i<power; i++)
+  {
+      acc *= base;
+  }
+
+  return acc;
+
 };
 
-const factorial = function() {
+const factorial = function(number) {
 	
+  if(number === 0)
+    return 1;
+
+  //else
+  return number *= factorial(number-1);
+  
 };
 
 // Do not edit below this line
