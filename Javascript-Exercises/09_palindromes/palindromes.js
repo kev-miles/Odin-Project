dde9f70c77518ec91d2b687ff4ba5ddfffbc1c6c
Kevin Miles <kev-miles@hotmail.com>
2022-05-01 18:59:26 +0100

    completed excercises 09 & 10

--- Javascript-Exercises/09_palindromes/palindromes.js
+++ Javascript-Exercises/09_palindromes/palindromes.js
@@ -1,6 +1,44 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    var input = removePunctuationAndSpaces(string.toLowerCase());
+    var reversedString = reverseString(input);
+
+    for(let i=0; i<input.length; i++)
+    {
+        if(input.charAt(i) != reversedString.charAt(i))
+            return false;
+    }
+
+    return true;
 };
+
+const removePunctuationAndSpaces = function(string){
+
+    var resultString = ""
+    var punctuation = [",",".","?","!"," "];
+
+    for(let i=0; i<string.length; i++)
+    {
+        if(punctuation.includes(string.charAt(i)))
+            continue;
+
+        resultString += string.charAt(i);
+    }
+
+    return resultString;
+}
+
+const reverseString = function(string)
+{
+    var resultString = "";
+
+    for(let i=string.length-1; i>=0; i--)
+    {
+        resultString += string.charAt(i);
+    }
+
+    return resultString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- Javascript-Exercises/10_fibonacci/fibonacci.js
+++ Javascript-Exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
 
+    var number = (typeof input === typeof "" ? Number(input) : input);
+
+    if(numberIsValid(number))
+    {
+        return calculateResult(number);
+    }
+
+    return "OOPS";
+};
+
+const calculateResult = function(number) {
+
+    if(number === 0 )
+        return 0;
+    else if(number === 1)
+        return 1;
+
+    return (calculateResult(number-1)+calculateResult(number-2));
+};
+
+const numberIsValid = function(number){
+
+    return (number > 0 ? true : false);
 };
 
 // Do not edit below this line
