69c580095d0fe780b67f6c5750efe39b26392e11
Kevin Miles <kev-miles@hotmail.com>
2022-05-02 09:49:35 +0100

    completed additional exercises

--- Javascript-Exercises/pigLatin/pigLatin.js
+++ Javascript-Exercises/pigLatin/pigLatin.js
@@ -1,6 +1,48 @@
 function pigLatin(string) {
 
+    var words = string.split(" ");
+
+    for(let i=words.length-1; i>=0; i--)
+    {
+        var word = (startsWithVowel(words[i]) ? words[i] + "ay" : findConsonants(words[i]) + "ay")
+        words[i] = word;
+    }
+
+    return words.join(" ");
+};
+
+function startsWithVowel(string)
+{
+    var vowels = 'aeiou'.split('');
+    return vowels.includes(string.charAt(0));
+};
+
+function findConsonants(string)
+{
+    var word = string;
+    var suffix = "";
+    var consonants = 'bcdfghjklmnpqrstvwxyz'.split('');
+
+    for(let i=0; i<string.length; i++)
+    {
+        if(!consonants.includes(string.charAt(i)) && isNotSpecialCase(string, i))
+            break;
+            
+        suffix += string.charAt(i);
+        word = word.substring(1);
+    }
+
+    return word += suffix;
 };
   
+function isNotSpecialCase(string, index)
+{
+    if(index > 0)
+        return (string.charAt(index-1) === "q" && string.charAt(index) === 'u' ? false : true);
+    
+    //esle
+    return false;
+}
+
 // Do not edit below this line
 module.exports = pigLatin;
--- Javascript-Exercises/snakeCase/snakeCase.js
+++ Javascript-Exercises/snakeCase/snakeCase.js
@@ -1,6 +1,52 @@
-const snakeCase = function() {
+const snakeCase = function(string) {
 
+    words = getWords(string);
+
+    for(let i= words.length-1; i>=0; i--)
+    {
+        var word = words[i];
+        words[i] = word.toLowerCase();
+    }
+
+    return validateString(words.join("_"));
 };
+
+function validateString(string) {
+   return string.replaceAll("__", "_");;
+}
+
+function getWords(string) {
+    var words = string.split(" ");
+    return (words.length > 1 ? ignorePunctuation(words) : handleJoinedWords(words))
+}
+
+function ignorePunctuation(words) {
+    for(let i=0; i<words.length; i++)
+    {
+        words[i] = words[i].replace(/[^\w\s\']|_/g, "");
+    }
+
+    return words;
+}
+
+function handleJoinedWords(joinedWord) {
+    var words = joinedWord[0].split(/[^\w\s\']|_/);
+    return (words.length>1 ? replacePunctuation(words) : splitOnCapitalLetter(joinedWord));
+}
+
+function replacePunctuation(words) {
+
+    for(let i=0; i<words.length; i++)
+    {
+        words[i] = words[i].replace(/[^\w\s\']|_/g, "_");
+    }
+
+    return words;
+}
+
+function splitOnCapitalLetter(joinedWord) {
+    return joinedWord[0].split(/(?=[A-Z])/)
+}
 
 // Do not edit below this line
 module.exports = snakeCase;
